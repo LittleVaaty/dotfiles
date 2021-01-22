4a5eff56c84653d90131c6dae38313633db3f7c8
Vaaty <hansen-jonathan@hotmail.com>
2021-01-22 09:34:48 +0100

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -2,22 +2,6 @@
 
 echo Install Package
 
-apt install vim
-apt install tmux
-apt install terminal
-apt install python-pip
-apt install tree
-apt install htop
-apt install figlet
-apt install zsh
-apt install curl 
-apt install cmus
-apt install rubygems
-apt install fd-find
-apt install shellcheck
-
-pip install terminal_velocity
+apt install vim terminator tree htop zsh curl cmus git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-gem install mdless
