317fbb28be3f41f8b12f567b9e543f0e6f688a6f
Jonathan Hansen <hansen-jonathan@hotmail.com>
2023-04-04 18:28:36 +0200

    Update install_dotfile.sh

--- install_dotfile.sh
+++ install_dotfile.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d "/$HOME/.config/nvim" ]
+then
+  echo '  [+] Create nvim config directory'
+  mkdir -p ~/.config/nvim
+fi
+
+
+
 dotfiles=(".vimrc" ".tmux.conf")
 
 dir="${HOME}/dotfiles"
