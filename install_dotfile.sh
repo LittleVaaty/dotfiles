6d14b52678663c1695a6c208aa11cd351c874822
Jonathan HANSEN <hnsjnt@forem.be>
2023-04-06 12:01:15 +0200

    Add zsh and oh-my-zsh config file

--- install_dotfile.sh
+++ install_dotfile.sh
@@ -9,7 +9,22 @@ for configFolder in "${configFolder[@]}"; do
   fi
 done
 
-dotfiles=("nvim/init.vim" "i3/config" "tmux/tmux.conf")
+dotfiles=("nvim/init.vim" "i3/config" "tmux/tmux.conf" "zsh/zshrc")
 for dotfile in "${dotfiles[@]}";do
  ln -sf "${HOME}/dotfiles/.config/${dotfile}" "${HOME}/.config/${dotfile}"
 done
+
+# Zsh installation
+
+ln -sf "$HOME/dorfile/.zshenv" "$HOME/.zshenv"
+
+if [! -d "/$HOME/.local/share/oh-my-zsh"]
+then
+    echo "  [+] Create oh-my-zsh data directory"
+    mkdir -p "/$HOME/.local/share/oh-my-zsh"
+fi
+
+echo "Install ho-my-zsh"
+ZSH="$HOME/.local/share/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
