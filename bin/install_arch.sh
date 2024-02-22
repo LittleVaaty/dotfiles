708dda1e5d78a32b0a43f4f719e9b949e24d3313
Jonathan HANSEN <hnsjnt@forem.be>
2024-02-22 15:19:54 +0100

    Update install arch

--- bin/install_arch.sh
+++ bin/install_arch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -d "/$HOME/.config/tmux/plugins/tpm" ]
+then
+  echo "install tmux pluggins manager"
+  mkdir -p ~/.config/tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+fi
+
 echo "Done !"
 
 
