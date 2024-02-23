69e8b6097afdbf2e0c8f116d9c651791b37dd98c
Jonathan HANSEN <hnsjnt@forem.be>
2024-02-23 08:37:05 +0100

    Update bootstrap script

--- bin/bootstrap-dotfiles.sh
+++ bin/bootstrap-dotfiles.sh
@@ -2,4 +2,23 @@
 
 echo "stowing dotfiles"
 
+read -r -p "Do you want stow GUI file? [y/N] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+  stow alacritty
+  stow i3
+  stow nitrogen
+  stow polybar
+  stow rofi
+fi
 
+stow lvim
+stow nvim
+stow ranger
+stow starship
+stow tmux
+stow Xresources
+stow zsh
+
+echo "Done !"
