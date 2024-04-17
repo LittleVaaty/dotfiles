4c7f7a5526e38d4fea39fc9e42176894364ca076
Jonathan Hansen <hansen-jonathan@hotmail.com>
2024-04-17 10:22:52 +0200

    Update install_wsl.sh
    add install of nodeJs

--- bin/install_wsl.sh
+++ bin/install_wsl.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/zsh
+#!/bin/bash
+
+function is_bin_in_path {
+  builtin type -P "$1" &> /dev/null
+}
 
 printf "\nUpdate Wsl"
 
@@ -15,6 +19,12 @@ sudo apt install -y \
   ranger \
   stow
 
+if ! is_bin_in_path nvm;
+then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  nvm install 20
+fi
+
 if [ ! -d "/$HOME/.config/tmux/plugins/tpm" ]
 then
   echo "install tmux pluggins manager"
@@ -22,7 +32,7 @@ then
   git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 fi
 
-cd $HOME
+cd "$HOME"
 
 echo "Install complete. Exit the shell and log back in to start the desktop environment."
 echo ""
